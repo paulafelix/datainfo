6c1d9d51b0f5c0b2f726c1537c45824651b6c449
paulafelix <p.felix@student.utwente.nl>
2014-05-06 16:00:00 +0200

    Schrijven stap 2

--- session2/result3d.sql
+++ session2/result3d.sql
@@ -1,1 +1,2 @@
-SELECT p.pid FROM Person p WHERE "is a writer who's movies are all directorless";
+SELECT p.pid FROM Person p WHERE EXISTS (
+SELECT * FROM Writes w WHERE p.pid = w.wid AND "written movies are all directorless");
