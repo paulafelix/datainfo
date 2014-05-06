10c64737dd5e719cd01ac6b41f7bca6720c87924
paulafelix <p.felix@student.utwente.nl>
2014-05-06 15:44:40 +0200

    Naam moest als resultaat ipv pid

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,3 +1,3 @@
-SELECT p.pid FROM Person p WHERE EXISTS (
+SELECT p.name FROM Person p WHERE EXISTS (
 SELECT * FROM Writes w WHERE p.pid = w.pid AND NOT EXISTS (SELECT * FROM DIRECTS d WHERE d.mid = w.mid)
 );
