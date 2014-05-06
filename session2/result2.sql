39c0ffd66be40309fa12a1f1ab6703825b335d4a
paulafelix <p.felix@student.utwente.nl>
2014-05-06 14:51:04 +0200

    Task 2.2b
    Splitsing in R1 en R2

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,3 +1,6 @@
 INSERT INTO F VALUES ("ABC", "EF", false, "Doordat je E weet weet je ook D, dus ABC is superkey"), 
 ("E", "D", true, "Als je E en D weet weet je ook A, maar dat maakt E nog geen superkey"), 
 ("D", "A", true, "Je weet alleen D en A, dus geen superkey");
+
+INSERT INTO F1 VALUES ("E", "D", false, "E is superkey omdat je dan ook D weet");
+INSERT INTO F2 VALUES ("ABC", "EF", false "Als je ABC weet weet je ook EF, dus superkey"), ("E", "A", true, "Als je E en A weet weet je B, C en F nog niet, dus geen superkey");
