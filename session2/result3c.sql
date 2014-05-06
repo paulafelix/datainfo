c119f57ba24f65da5c32006c9d32acf1ab2c4933
paulafelix <p.felix@student.utwente.nl>
2014-05-06 15:43:46 +0200

    Stap 3 schrijven

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,3 +1,3 @@
 SELECT p.pid FROM Person p WHERE EXISTS (
-SELECT * FROM Writes w WHERE p.pid = w.pid AND 'mid has no director'
+SELECT * FROM Writes w WHERE p.pid = w.pid AND NOT EXISTS (SELECT * FROM DIRECTS d WHERE d.mid = w.mid)
 );
