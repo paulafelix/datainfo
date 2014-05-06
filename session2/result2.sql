48fbb39e11d0b6627c34263d8412705f6a0549ca
paulafelix <p.felix@student.utwente.nl>
2014-05-06 14:57:10 +0200

    Opgave 2.2 taak c
    R2 gesplitst in R2a en R2b

--- session2/result2.sql
+++ session2/result2.sql
@@ -3,4 +3,6 @@ INSERT INTO F VALUES ("ABC", "EF", false, "Doordat je E weet weet je ook D, dus 
 ("D", "A", true, "Je weet alleen D en A, dus geen superkey");
 
 INSERT INTO F1 VALUES ("E", "D", false, "E is superkey omdat je dan ook D weet");
-INSERT INTO F2 VALUES ("ABC", "EF", false "Als je ABC weet weet je ook EF, dus superkey"), ("E", "A", true, "Als je E en A weet weet je B, C en F nog niet, dus geen superkey");
+INSERT INTO F2 VALUES ("ABC", "EF", false, "Als je ABC weet weet je ook EF, dus superkey"), ("E", "A", true, "Als je E en A weet weet je B, C en F nog niet, dus geen superkey");
+INSERT INTO F2a VALUES ("E", "A", false, "E is superkey, want je weet dan ook A");
+INSERT INTO F2b VALUES ("EBC", "F", false, "Als je EBC weet, weet je ook F");
