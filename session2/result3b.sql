0d6782de7585b19bb907bd1af81854fbcd486cf9
paulafelix <p.felix@student.utwente.nl>
2014-05-06 15:36:12 +0200

    Used the shunting rule again

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,5 +1,1 @@
-SELECT p.pid FROM Person p WHERE EXISTS(
-            SELECT DISTINCT * FROM Acts a, Movies m
-            WHERE a.pid = p.pid
-            AND m.mid = a.mid AND m.name = 'Back to the Future'
-);
+SELECT DISTINCT p.pid FROM Person p, Acts a, Movies m WHERE a.pid = p.pid AND m.mid = a.mid AND m.name = 'Back to the Future';
